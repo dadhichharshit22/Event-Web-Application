c16726471039bda63e5d3f089f6f47dada9db6f2
dadhichharshit22 <dadhich.harshit22122002@gmail.com>
2024-12-23 21:46:25 +0530

    Initial Commit

--- config/database.tsx
+++ config/database.tsx
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+// connec the database 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI!);
